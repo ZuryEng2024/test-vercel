11a4648c0adb46681962a4899b6596dcaaae7c65
ZurisadaiHS <zury_hs@hotmail.com>
2026-05-05 13:49:54 -0600

    segunda pagina

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import {resolve} from 'path'
+
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rolluoOption: {
+        input: {
+            main: resolve(__dirname, 'index.html'),
+            prueba: resolve(__dirname, 'prueba.html')
+        }
+    }
+  }
 })
